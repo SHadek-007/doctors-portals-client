9e81a5673cff2a5309237078bf18f24982a17a1b
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-11 12:29:56 +0600

    modal done

--- src/Pages/Appoinment/AppointService.js
+++ src/Pages/Appoinment/AppointService.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-const AppointService = ({ service }) => {
+const AppointService = ({ service, setTreatment}) => {
     const {name, slots} = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
       <div className="card-body text-center">
-        <h2 className="text-2xl text-center text-secondary font-bold">{name}</h2>
+        <h4 className="text-xl text-center text-secondary">{name}</h4>
         <p><small>
             {slots.length > 0 ? <span>{slots[0]}</span> : <span className="text-red-400">No Slot Available</span>}
         </small></p>
         <p><small>{slots.length} {slots.length > 1 ? 'SPACES' : "SPACE"} AVAILABLE</small></p>
         <div className=" justify-center">
-          <button disabled={slots.length === 0} className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Book Appointment</button>
+          <label htmlFor="booking-modal" 
+          onClick={()=> setTreatment(service)} disabled={slots.length === 0}
+          className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
+
         </div>
       </div>
     </div>
--- src/Pages/Appoinment/AvailableAppointment.js
+++ src/Pages/Appoinment/AvailableAppointment.js
@@ -1,9 +1,12 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import AppointService from './AppointService';
+import BookingModal from './BookingModal';
 
 const AvailableAppointment = ({date}) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
     useEffect(()=>{
         fetch('services.json')
         .then(res => res.json())
@@ -15,9 +18,10 @@ const AvailableAppointment = ({date}) => {
             <h4 className='text-2xl text-secondary text-center mb-8'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-5 lg:px-12'>
                 {
-                    services.map(service => <AppointService key={service._id} service={service}></AppointService>)
+                    services.map(service => <AppointService key={service._id} service={service} setTreatment={setTreatment}></AppointService>)
                 }
             </div>
+            {treatment && <BookingModal date={date} treatment={treatment} setTreatment={setTreatment}></BookingModal>}
         </div>
     );
 };
